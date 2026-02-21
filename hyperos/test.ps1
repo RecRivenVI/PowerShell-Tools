6092eab87260b9a44a9a9e382b35ef9390c68547
RavenYin <178878631@qq.com>
2026-02-21 21:35:27 +0800

    更新

--- hyperos/test.ps1
+++ hyperos/test.ps1
@@ -1,12 +1,23 @@
 $ExistedTgzs = Get-ChildItem -Path "D:\tgzs" -File
+$ExistedNames = @($ExistedTgzs.Name)
 $DeviceList = Get-Content -Path "$PSScriptRoot\devices.txt"
 
 $DeviceList |
 ForEach-Object -Parallel { 
-    $json = (Invoke-WebRequest -Uri "https://update.intl.miui.com/updates/miota-fullrom.php?d=$_&b=F&r=cn") | ConvertFrom-Json
-    if ($using:ExistedTgzs.Name -contains ($json.LatestFullRom.filename -replace " ","")) {
-        Write-Host "$($json.LatestFullRom.version)已存在|$($json.LatestFullRom.device)"
-    } else {
-        Write-Host ("$($json.LatestFullRom.version)已更新|$($json.LatestFullRom.device)|https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/$($json.LatestFullRom.version)/$($json.LatestFullRom.filename)" -replace " ","")
+    function Test-RomStatus {
+        if ($using:ExistedNames -contains ($json.LatestFullRom.filename -replace " ", "")) {
+            Write-Host ("已存在 | $($json.LatestFullRom.version) | $($json.LatestFullRom.device)" -replace "V816", "OS1")
+        }
+        else {
+            Write-Host ("可更新 | $($json.LatestFullRom.version) | $($json.LatestFullRom.device) |" -replace "V816", "OS1")(("https://bkt-sgp-miui-ota-update-alisgp.oss-ap-southeast-1.aliyuncs.com/$($json.LatestFullRom.version)/$($json.LatestFullRom.filename)" -replace " ", "") -replace "V816", "OS1")
+        }
+    }
+    if ($_ -eq "clover") {
+        $json = (Invoke-WebRequest -Uri "https://update.intl.miui.com/updates/miota-fullrom.php?d=$_&b=X&r=cn") | ConvertFrom-Json
+        Test-RomStatus
+    }
+    else {
+        $json = (Invoke-WebRequest -Uri "https://update.intl.miui.com/updates/miota-fullrom.php?d=$_&b=F&r=cn") | ConvertFrom-Json
+        Test-RomStatus
     }
 } -ThrottleLimit 8
